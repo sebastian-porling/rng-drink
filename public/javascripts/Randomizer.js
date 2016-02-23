336e68070dd88dc30e9c583668894cb3299a12e6
Steven <sly@kth.se>
2016-02-23 21:13:26 +0100

    Added a javafile which reads from file and sorts the spirits and mixers, also added an illegal combination list

---
+++ public/javascripts/Randomizer.js
@@ -0,0 +1,95 @@
+/**
+ * Created by steve_000 on 2016-02-23.
+ */
+
+var mixer,
+    illegal = {},
+    spirits,
+    extra = [ 'lime', 'citron' ],
+    fs = require('fs'),
+    path = require('path'),
+    filePath = path.join(__dirname, '../resources/groggvirke.txt'),
+    filePath2 = path.join(__dirname, '../resources/sprit.txt');
+
+
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err){
+        //console.log('received data: ' + data);
+        mixer = data.split('\r\n');
+
+        for(var i = 0; i < mixer.length; i++){
+            if(mixer[i].indexOf("#") != -1){
+                var tmp = mixer[i].split("#");
+
+                illegal[tmp[0]] = tmp[1].split(" ");
+                mixer[0] = tmp[0];
+            }
+        }
+
+        mixer.forEach(function(str){
+
+        });
+        console.log(illegal);
+    }else{
+        console.log(err);
+    }
+});
+
+fs.readFile(filePath2, {encoding: 'utf-8'}, function(err,data){
+    if (!err){
+        //console.log('received data: ' + data);
+        spirits = data.split('\r\n');
+        for(var i = 0; i < spirits.length; i++){
+            if(spirits[i].indexOf("#") != -1){
+                var tmp = spirits[i].split("#");
+
+                illegal[tmp[0]] = tmp[1].split(" ");
+                spirits[0] = tmp[0];
+            }
+        }
+    }else{
+        console.log(err);
+    }
+});
+
+
+var Init = function(){
+    // Read in all mixers and all spirits
+    fs.open('./resources/groggvirke', 'r',function(err, fileToRead){
+
+    });
+};
+
+var test = function(res){
+    var str = randomMixer() + " with " + randomSpirit();
+
+    res.send(str);
+}
+
+var toString = function(){
+    var keyvalues = {};
+    keyvalues["Mixer"] = mixer;
+    keyvalues["Spirits"] = spirits;
+    keyvalues["Illegal"] = illegal;
+
+    return keyvalues;
+}
+
+function randomSpirit(){
+    var i = getRandomInt(0, spirits.length);
+
+    return spirits[i];
+}
+
+function randomMixer(){
+    var i = getRandomInt(0, mixer.length);
+
+    return mixer[i];
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+exports.toString = toString;
+exports.test = test;
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next){
+  randomizer.test(res);
+});
+
+router.get('/toString', function(req, res, next){
+  res.send(randomizer.toString());
+});
+
+module.exports = router;
