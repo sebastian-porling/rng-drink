4c11901a7b849324cba47735e927776218891987
Steven <sly@kth.se>
2016-02-23 22:58:49 +0100

    Tweaked some of the randomness, preventing some milkproducts to be mixed with lime or lemon

--- public/javascripts/Randomizer.js
+++ public/javascripts/Randomizer.js
@@ -60,61 +60,94 @@ var drink = function(size, res){
 
     drink["spirits"] = getSpirits(size);
     drink["mixers"] = getMixers(size);
-    drink["extra"] = getExtra();
+    drink["extra"] = getExtra(drink["spirits"], drink["mixers"]);
 
     res.send(drink);
-};
+}
 
 var cocktail = function(size, res){
     var cocktail = {};
     cocktail["spirits"] = getSpirits(size);
     cocktail["extra"] = getExtra();
     res.send(drink);
-};
+}
 
-
+// Main help functions
 function getSpirits(size){
     var spirits = [];
     console.log(size + "cl");
+
+    // Size of the drink
     while(size > 0){
+
         var spirit = {};
+
+        // Randomize a spirit
         spirit["name"] = randomSpirit();
+
+
+        // Randomize how much of the spirit we gonna take
         var amount = getRandomInt(1, parseInt(size)+1);
         spirit["amount"] = amount;
+
+        // Add new spirit to spirit list
         spirits = spirits.concat([spirit]);
+
+        // Reduce how much spirit left
         size = size - amount;
     }
     return spirits;
-};
+}
 
 function getMixers(size){
     var mixers = [];
+
     size =  Math.round(parseInt(size)/2);
-    console.log(size + " mixers");
+    console.log(size + " mixers at most");
+
     var i = 0;
-    while(getRandomInt(10-size, 30+i) < 20 || i == 0){
+    // The higher amount of spirits increases chances for mixers
+    // The more mixers it already contains, the less chance will it be for a new one
+    while((getRandomInt(0, 1000)%(5-size + i) == 0|| i == 0) && i < size){
         mixers = mixers.concat([randomMixer()]);
-        i += 5;
+        i +=1;
     }
 
     return mixers;
-};
+}
 
-function getExtra(){
+function getExtra(spirits, mixers){
+    var i;
+    for(i = 0; i < spirits.length; i++){
+        if(illegal[spirits[i].name] != null){
+            console.log("Contains milk don't add extra");
+            return;
+
+        }
+    }
+    for(i = 0; i < mixers.length; i++){
+        if(illegal[mixers[i]] != null){
+            console.log("Contains milk don't add extra");
+            return;
+
+        }
+    }
+
+
     var extra = [];
-    if(getRandomInt(0,100)%5 == 0){
+    // 1 of 5 chances to add lime or lemon
+    if(getRandomInt(0,1000)%5 == 0){
         extra = randomExtra();
     }
     return extra;
-};
-
+}
 
 // Test functions
 var test = function(res){
     var str = randomMixer() + " with " + randomSpirit();
 
     res.send(str);
-};
+}
 
 var toString = function(){
     var keyvalues = {};
@@ -123,7 +156,7 @@ var toString = function(){
     keyvalues["Illegal"] = illegal;
 
     return keyvalues;
-};
+}
 
 
 // Help functions
@@ -131,24 +164,24 @@ function randomSpirit(){
     var i = getRandomInt(0, spirits.length);
 
     return spirits[i];
-};
+}
 
 function randomMixer(){
     var i = getRandomInt(0, mixer.length);
 
     return mixer[i];
-};
+}
 
 function randomExtra(){
     var i = getRandomInt(0, extra.length);
 
     return extra[i];
-};
+}
 
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 exports.toString = toString;
 exports.test = test;
