a0002d100392d3e99c37ef0202b54ccc8d16a1ce
Steven <sly@kth.se>
2016-02-26 00:42:33 +0100

    Displays properly. not json anymore, easier for humans to read. Now properly checks things that should not be mixed together. No more duplicates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(),
+    history = "";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('./public/index.html', {root: __dirname });
 });
 
 router.get('/test', function(req, res, next){
@@ -14,12 +15,61 @@ router.get('/toString', function(req, res, next){
   res.send(randomizer.toString());
 });
 
+router.get('/history', function(req, res, next){
+  var str = "\<div\>" +  "History:" + "\</div\>";
+  str += history;
+  res.send(str);
+});
+
 router.get('/drink/:amount', function(req, res, next){
-  randomizer.drink(req.params.amount, res);
+
+  var drink = randomizer.drink(req.params.amount);
+  var str = "\<div\>" +  "Liquor: " + drink.alcohol + " cl" + "\</div\>";
+  drink.spirits.forEach(function(spirit){
+    str += "\<div\>" + spirit.amount + "cl" + " " + spirit.name  + "\</div\>";
+  });
+  str +=  "\<div\>" + "_" + "\</div\>";
+  str +=  "\<div\>" +  "Mixers:" + "\</div\>";
+
+  drink.mixers.forEach(function(mixer){
+    str += "\<div\>" + mixer + "\</div\>";
+  });
+  console.log("extra " + drink.extra);
+  if(drink.extra != undefined && drink.extra.length > 0) {
+    str +=  "\<div\>" + "_" + "\</div\>";
+    str += "\<div\>" + "Extra:" + "\</div\>";
+
+    str += "\<div\>" + drink.extra + "\</div\>";
+
+  }
+
+  res.send(str);
+
+  history +=  "\<hr\>";
+  history += "\<div\>" + "Drink:" + "\</div\>";;
+  history += str;
 });
 
 router.get('/cocktail/:amount', function(req, res, next){
-  randomizer.cocktail(req.params.amount, res);
+  var drink = randomizer.cocktail(req.params.amount);
+
+  var str = "\<div\>" +  "Liquor:" + drink.alcohol + " cl" + "\</div\>";
+  drink.spirits.forEach(function(spirit){
+    str += "\<div\>" + spirit.amount + "cl" + " " + spirit.name  + "\</div\>";
+  });
+
+  if(drink.extra != undefined && drink.extra.length > 0) {
+    str +=  "\<div\>" + "_" + "\</div\>";
+    str += "\<div\>" + "Extra:" + "\</div\>";
+
+    str += "\<div\>" + drink.extra + "\</div\>";
+
+  }
+
+  res.send(str);
+  history +=  "\<hr\>";
+  history += "\<div\>" + "Cocktail:" + "\</div\>";;
+  history += str;
 });
 
 module.exports = router;
